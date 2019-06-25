06822aafc314cfdccdb7df7e902931f2669a55f6
Rachael Esler <rachael.esler@gmail.com>
2019-06-25 11:37:01 +1000

    Included addons: knobs and notes

--- .storybook/config.js
+++ .storybook/config.js
@@ -3,6 +3,12 @@
 import { configure } from '@storybook/react';
 import '../src/index.css';
 
+// Addons
+import '@storybook/addon-actions/register';
+import '@storybook/addon-knobs/register';
+import '@storybook/addon-notes/register';
+import '@storybook/addon-knobs/register';
+
 const req = require.context('../src', true, /.stories.js$/);
 
 function loadStories() {
